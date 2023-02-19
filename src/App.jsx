c6c5318835edb83c3d445c255943add9962a4902
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-02-19 20:26:24 +0530

    navbar section completed with web and mobile.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
+import NavBar from "./components/navbar";
+
 function App() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-center text-3xl font-bold text-neutral_very_dark_violet">
-        url shortener
-      </h1>
+    <div className="container mt-8 px-3 lg:mx-auto">
+      <NavBar />
     </div>
   );
 }
