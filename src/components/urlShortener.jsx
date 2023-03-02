73368618212010d2b72e0c312df42b7f7b8cd5e4
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-03-02 18:38:34 +0530

    validation in url added

--- src/components/urlShortener.jsx
+++ src/components/urlShortener.jsx
@@ -2,6 +2,16 @@ import React, { useState } from "react";
 
 function UrlShortener({ handleClick }) {
   const [url, setUrl] = useState("");
+  const validateUrl = (url) => {
+    const regex = new RegExp(
+      "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?"
+    );
+    if (regex.test(url)) {
+      setUrl(url);
+    } else {
+      setUrl("");
+    }
+  };
   return (
     <div className="container absolute top-[-4rem] rounded-lg bg-primary_dark_violet bg-shortenMobile bg-cover lg:flex lg:h-[150px] lg:w-full lg:items-center lg:justify-center lg:bg-shortenDeskTop">
       <div className="flex flex-col gap-3 px-5 py-6 lg:w-full lg:flex-row lg:items-center lg:justify-evenly lg:gap-5 lg:px-12">
@@ -10,11 +20,12 @@ function UrlShortener({ handleClick }) {
           name="longURL"
           placeholder="Shorten a link here..."
           className="rounded-lg py-3 px-5 lg:basis-3/4"
-          onChange={(e) => setUrl(e.target.value)}
+          onChange={(e) => validateUrl(e.target.value)}
         />
         <button
           type="button"
-          className="rounded-lg bg-primary_cyan px-8 py-3 text-base font-bold text-white hover:bg-hover lg:basis-1/4"
+          disabled={url === ""}
+          className="rounded-lg bg-primary_cyan px-8 py-3 text-base font-bold text-white hover:bg-hover disabled:cursor-not-allowed disabled:opacity-70 disabled:hover:bg-primary_cyan lg:basis-1/4"
           onClick={() => handleClick(url)}
         >
           Shorten it!
